28688266cbbddabd8690855f1cabbf4f82a1b4bd
てんでん <barururu3.pororo.kmt@gmail.com>
2024-04-05 02:16:18 +0900

    canvasがvercelで動くように

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+module.exports = (phase, { defaultConfig }) => {
+  if (
+    process.env.LD_LIBRARY_PATH == null ||
+    !process.env.LD_LIBRARY_PATH.includes(
+      `${process.env.PWD}/node_modules/canvas/build/Release:`
+    )
+  ) {
+    process.env.LD_LIBRARY_PATH = `${
+      process.env.PWD
+    }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
+  }
 
-export default nextConfig;
+  return {
+    ...defaultConfig,
+    reactStrictMode: true,
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        config.externals.push("canvas");
+      }
+      return config;
+    },
+  };
+};
