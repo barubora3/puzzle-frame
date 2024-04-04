02199c879ab64a42f9d3714a24e889831a6bb798
てんでん <barururu3.pororo.kmt@gmail.com>
2024-04-05 02:33:13 +0900

    canvas周りの設定を切り戻し

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push("canvas");
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
